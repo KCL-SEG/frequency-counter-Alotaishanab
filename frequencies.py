e902441677d3e0c08c7a31bd8c47c84c5f800e56
Alotaishanab <114522412+Alotaishanab@users.noreply.github.com>
2022-09-30 17:32:10 +0100

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,16 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def frequencies(items):
-    frequencies = {}
-    # Your code goes here
-    return frequencies
+    frequency = {}
+    for j in items:   #iterate through the items
+        j = str(j)    #convert values in items to a string 
+        if j in frequency.keys(): #if the item is in the dictionary
+            frequency[j] += 1 #increment the value 
+        else:
+            frequency[j] = 1 #if item is not in dictionary equal it to one
+    return frequency
+
+
+print(frequencies(['chocolate','chocolate','chocolate','3',3,3,3,2,2]))
